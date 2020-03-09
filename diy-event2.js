8af48420b91f6956784ac830ee04dcc25b8a3bbe
mumura <61147948+mumura@users.noreply.github.com>
2020-03-09 23:43:11 +0900

    Add files via upload

---
+++ diy-event2.js
@@ -0,0 +1,21 @@
+function showDetail (){
+document.querySelector('#open').style.display = "none";
+document.querySelector('#desc').style.display = "block";
+}
+function hideDetail (){
+document.querySelector('#open').style.display = "block";
+document.querySelector('#desc').style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    
