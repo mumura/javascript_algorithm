58efb12696d843444456f75c73382d88ba886d6e
mumura <61147948+mumura@users.noreply.github.com>
2020-03-11 23:47:25 +0900

    기념일 만들기 ing ver2

--- dday.js
+++ dday.js
@@ -7,17 +7,16 @@ var passedTime = toNow - toFirst;
 var passedDay = Math.round(passedTime/(1000*60*60*24));
 document.querySelector("#accent").innerText = passedDay + "일";
 
-
-var future = toFirst + 100*(1000*60*60*24); 
+function calcDate(days) {
+var future = toFirst + days*(1000*60*60*24); 
 var someday = new Date(future); 
 var year = someday.getFullYear( ); 
 var month = someday.getMonth( );
 var date = someday.getDate( ); 
-document.querySelector("#date100").innerText = year + "년 " + month + "월 " + date + "일";
+document.querySelector("#date" + days).innerText = year + "년 " + month + "월 " + date + "일";
+}
 
-var future = toFirst + 200*(1000*60*60*24); 
-var someday = new Date(future); 
-var year = someday.getFullYear( ); 
-var month = someday.getMonth( );
-var date = someday.getDate( ); 
-document.querySelector("#date200").innerText = year + "년 " + month + "월 " + date + "일";
+calcDate(129);
+calcDate(396);
+calcDate(6971);
+calcDate(8432);
